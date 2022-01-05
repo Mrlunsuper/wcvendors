a9aea81c7b98312b7ad9c8a7674ec4e243f75e60
digitalchild <digitalchild@gmail.com>
2022-01-05 15:50:00 +0700

    Updated: Only show the mark shipped button if vendors haven't marked shipped

--- classes/admin/class-wcv-admin-orders.php
+++ classes/admin/class-wcv-admin-orders.php
@@ -25,7 +25,7 @@ class WCVendors_Admin_Orders {
         add_action( 'admin_notices', array( $this, 'admin_notices' ) );
 		add_action( 'woocommerce_admin_order_data_after_shipping_address', array( $this, 'add_vendor_shipped_details'), 10, 2 );
 		add_action( 'woocommerce_admin_order_actions', array( $this, 'append_mark_shipped' )   , 10, 2 );
-		add_action( 'wp_ajax_wcvendors_mark_order_shipped', array( __CLASS__, 'mark_order_all_shipped' ) );
+		add_action( 'wp_ajax_wcvendors_mark_order_shipped', array( __CLASS__, 'mark_order_shipped' ) );
     }
 
     /**
@@ -46,7 +46,9 @@ class WCVendors_Admin_Orders {
 	 */
 	public function append_mark_shipped( $actions, $order ) { 
 
-		if ( $order->has_status( array( 'processing', 'completed' ) ) ) {
+        $this->all_vendors_shipped( $order );
+
+		if ( $order->has_status( array( 'processing', 'completed' ) ) &&  ! $this->all_vendors_shipped( $order ) ) {
 			$actions['wcvendors_mark_shipped'] = array(
 				'url'    => wp_nonce_url( admin_url( 'admin-ajax.php?action=wcvendors_mark_order_shipped&order_id=' . $order->get_id() ), 'wcvendors-mark-order-shipped' ),
 				'name'   => __( 'Mark Shipped', 'wc-vendors' ). $this->get_vendors_shipped_text( $order ),
@@ -85,10 +87,30 @@ class WCVendors_Admin_Orders {
 		return $string;
 	}
 
+    /**
+     * Check of all vendors have shipped for the order
+     *
+     * @param WC_Order $order The order to check
+     * @return boolean $all_shipped if all vendors have shipped
+     */
+    public function all_vendors_shipped( $order ){
+        $vendor_ids = array_keys( WCV_Vendors::get_vendors_from_order( $order ) );
+        $shipped = array_filter( (array) get_post_meta( $order->get_id(), 'wc_pv_shipped', true ) );
+
+        if ( empty( array_diff( $vendor_ids, $shipped ) ) ){
+            WC_Vendors::log( 'Order ID: #'. $order->get_id(). ' all shipped.' );
+        }
+
+        $all_shipped = empty( array_diff( $vendor_ids, $shipped ) );
+
+        return $all_shipped;
+
+    }
+
 	/**
 	 * Mark an order shipped for all vendors.
 	 */
-	public static function mark_order_all_shipped() {
+	public static function mark_order_shipped() {
 		if ( current_user_can( 'edit_shop_orders' ) && check_admin_referer( 'wcvendors-mark-order-shipped' ) && $_GET['order_id'] )  {
 
             $order  = wc_get_order( absint( wp_unslash( $_GET['order_id'] ) ) );
@@ -135,5 +157,3 @@ class WCVendors_Admin_Orders {
     }
 
 }
-
-
