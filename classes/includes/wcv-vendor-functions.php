06e621e6d7a8ba9d326b2273f8df00a0dad3f35e
Mrlunsuper <irisslee@gmail.com>
2022-05-11 16:28:39 +0700

    Update avatar size type

--- classes/includes/wcv-vendor-functions.php
+++ classes/includes/wcv-vendor-functions.php
@@ -158,7 +158,7 @@ if ( ! function_exists( 'wcv_get_vendor_avatar' ) ) {
 	function wcv_get_vendor_avatar( $vendor_id ) {
 
 		$avatar_source = get_option( 'wcvendors_display_vendors_avatar_source', 'mystery' );
-		$avatar_size   = apply_filters( 'wcvendors_vendor_avatar_size', '200' );
+		$avatar_size   = apply_filters( 'wcvendors_vendor_avatar_size', 200 );
 
 		$vendor_avatar = get_avatar( $vendor_id, $avatar_size, $avatar_source, '', array( 'class' => 'wcv-avatar' ) );
 		return apply_filters( 'wcvendors_vendor_avatar', $vendor_avatar, $vendor_id );
