2c6863579bbf5e5aea53a4b713a2ed0481a85f6a
Mrlunsuper <irisslee95@gmail.com>
2022-06-24 12:13:27 +0700

    Update hyperlink and css

--- templates/front/vendor-list-loop.php
+++ templates/front/vendor-list-loop.php
@@ -28,7 +28,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<a href="<?php echo esc_url( $shop_link ); ?>"><?php echo $avatar; ?></a>
 	</div>
 	<div class="vendor_list_info">
-		<a href="<?php echo esc_url( $shop_link ); ?>"><h3 class="vendor_list--shop-name"><?php echo esc_html( $shop_name ); ?></h3></a>
+		<h3 class="vendor_list--shop-name">
+			<a href="<?php echo esc_url( $shop_link ); ?>"><?php echo esc_html( $shop_name ); ?></a>
+		</h3>
 		<small class="vendors_list--shop-phone"><span class="dashicons dashicons-smartphone"></span><span><?php echo esc_html( $phone ); ?></span></small> <br/>
 		<small class="vendors_list--shop-address"><span class="dashicons dashicons-location"></span><span><?php echo esc_html( $address ); ?></span></small><br/>
 		<a class="button vendors_list--shop-link" href="<?php echo esc_url( $shop_link ); ?>"><?php esc_html_e( 'Visit Store', 'wc-vendors' ); ?></a>
